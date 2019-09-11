0377734e2b839f5499f61817f9b12e91651edcd9
matthewhopwood <matthewhopwood21@hotmail.com>
2019-09-11 08:41:39 +0100

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+  # as: - this allows a custom path to be created, so the example would be 'portfolio_show_path(@portfolio_item)'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
+  # get 'anything can go here' - this is the url path.
+  # to: 'pages#about' - links the url path to the pages#show action.
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
