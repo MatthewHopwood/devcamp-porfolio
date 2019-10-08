177e19c2ddd1b858f7787c615e69abdd55d32bae
matthewhopwood <matthewhopwood21@hotmail.com>
2019-10-08 13:00:06 +0100

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    DdarkbluuViewTool::Renderer.copyright 'Matthew Hopwood', 'All rights reserved'
+  end
 end
