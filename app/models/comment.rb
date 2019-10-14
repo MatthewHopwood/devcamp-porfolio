6457565a72494b49740902b5d350461052df3c1c
matthewhopwood <matthewhopwood21@hotmail.com>
2019-10-14 08:27:03 +0100

    Added custom scope order DESC to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,8 @@ class Comment < ApplicationRecord
   validates :content, presence: true, length: { minimum: 5, maximum: 1000 }
 
   after_create_commit { CommentBroadcastJob.perform_later(self) }
+
+  def self.recent
+    order('created_at DESC')
+  end
 end
